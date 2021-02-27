7eeb690c822d4fee2e74e1000a0a378ac3d12fc9
sabil62 <shresthasabil62@gmail.com>
2021-02-27 22:53:20 +0545

    clock animation completed

--- lib/clock_view.dart
+++ lib/clock_view.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:math';
 
 import 'package:flutter/material.dart';
@@ -9,24 +10,36 @@ class ClockView extends StatefulWidget {
 
 class _ClockViewState extends State<ClockView> {
   @override
+  void initState() {
+    Timer.periodic(Duration(milliseconds: 1000), (timer) {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
       height: 300,
       width: 300,
-      child: CustomPaint(
-        painter: ClockPainter(),
+      child: Transform.rotate(
+        angle: -pi / 2,
+        child: CustomPaint(
+          painter: ClockPainter(),
+        ),
       ),
     );
   }
 }
 
 class ClockPainter extends CustomPainter {
+  var dateTime = new DateTime.now();
   @override
   void paint(Canvas canvas, Size size) {
-    var sizeX = size.width / 2;
-    var sizeY = size.height / 2;
-    var center = Offset(sizeX, sizeY);
-    var radius = min(sizeX, sizeY);
+    var centerX = size.width / 2;
+    var centerY = size.height / 2;
+    var center = Offset(centerX, centerY);
+    var radius = min(centerX, centerY);
 
     var strokeCircle = Paint()
       ..color = Color(0xFFEAECFF)
@@ -39,26 +52,46 @@ class ClockPainter extends CustomPainter {
       ..color = Colors.orange[300]
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 16;
+      ..strokeWidth = 8;
     var minHand = Paint()
       ..shader = RadialGradient(
               colors: [Colors.lightBlue, Colors.pink]) //not color ..shader
           .createShader(Rect.fromCircle(center: center, radius: radius))
       ..style = PaintingStyle.stroke
       ..strokeCap = StrokeCap.round
-      ..strokeWidth = 16;
+      ..strokeWidth = 14;
     // .createShader(Rect.fromCircle(center: center, radius: radius));
+
+    var hourHand = Paint()
+      ..shader = RadialGradient(colors: [Color(0xFFEA74AB), Color(0xFFC279FB)])
+          .createShader(Rect.fromCircle(center: center, radius: radius))
+      ..style = PaintingStyle.stroke
+      ..strokeCap = StrokeCap.round
+      ..strokeWidth = 16;
 
     canvas.drawCircle(center, radius - 40, mainCenter);
     canvas.drawCircle(center, radius - 40, strokeCircle);
     //the small circle z-index is greater then hands
-    canvas.drawLine(center, Offset(100, 100), minHand);
-    canvas.drawLine(center, Offset(200, 100), secHand);
+    //the maths is simple (centerPoint + lengthOfHand * rotation)
+    //(pi / 180) is 1 degree hai in all 360 degree rotation
+    var minHandX = centerX + 80 * cos(dateTime.minute * 6 * (pi / 180));
+    //(pi/180 is 1degree)
+    var minHandY = centerY + 80 * sin(dateTime.minute * 6 * (pi / 180));
+    canvas.drawLine(center, Offset(minHandX, minHandY), minHand);
+    var secHandX = centerX + 80 * cos(dateTime.second * 6 * pi / 180);
+    var secHandY = centerX + 80 * sin(dateTime.second * 6 * pi / 180);
+    canvas.drawLine(center, Offset(secHandX, secHandY), secHand);
+    //EACH minute le half degree badauxa
+    var hourHandX = centerX +
+        60 * cos((dateTime.hour * 30 + dateTime.minute * 0.5) * (pi / 180));
+    var hourHandY = centerX +
+        60 * sin((dateTime.hour * 30 + dateTime.minute * 0.5) * (pi / 180));
+    canvas.drawLine(center, Offset(hourHandX, hourHandY), hourHand);
     canvas.drawCircle(center, 16, smallCenter);
   }
 
   @override
   bool shouldRepaint(covariant CustomPainter oldDelegate) {
-    throw UnimplementedError();
+    return true;
   }
 }
